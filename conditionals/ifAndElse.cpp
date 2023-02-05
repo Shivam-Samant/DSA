6deabef8f8867b95bd6e2b138cffa64db32ec37e
Shivam-Samant <shivamsamant000@gmail.com>
2023-02-05 14:05:58 +0530

    conditionals

---
+++ conditionals/ifAndElse.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define IOS ios_base::sync_with_stdio(0)
+#define tie cin.tie(NULL),cout.tie(NULL)
+#define ll long long
+#define testcases ll T; cin >>T; while(T--)
+#define fi(i,a,b) for(ll i = a; i < b; i++)
+#define fd(i,a,b) for(ll i = a; i > b; i--)
+#define vc vector
+#define vi vc<int>
+#define vll vc<ll>
+#define pb push_back
+#define pii pair<int,int>
+#define mk make_pair
+#define all(p) p.begin(), p.end()
+#define si set<int>
+#define mii map<int, int>
+#define endl '\n'
+
+int main(){
+	int age;
+	cin >> age;
+	if (age >= 18) {
+		cout << "You can vote";
+	} else {
+		cout << "You can't vote";
+	}
+	return 0;
+}
