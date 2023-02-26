7b9ead8ba37013395305d336f9e7573e6b1e2499
Shivam-Samant <shivamsamant000@gmail.com>
2023-02-26 12:49:02 +0530

    remove duplicate adjacent element in string problem solved

---
+++ string/removeSameAdjacentElm.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define IOS ios_base::sync_with_stdio(0)
+#define tie cin.tie(NULL),cout.tie(NULL)
+#define ll long long
+#define testcases ll T; cin >>T; while(T--)
+#define fi(i,a,b) for(ll i = a; i < b; i++)
+#define fd(i,a,b) for(ll i = a; i > b; i--)
+#define vc vector
+#define vi vc<int>
+#define vll vc<ll>
+#define pb push_back
+#define pii pair<int,int>
+#define mk make_pair
+#define all(p) p.begin(), p.end()
+#define si set<int>
+#define mii map<int, int>
+#define endl '\n'
+
+int main(){
+  string s = "abbazza";
+  string ans;
+  ans.push_back(s[0]);
+  for(int i = 1; i < s.length(); i++) {
+    if (ans.length()-1 >= 0 && s[i] == ans[ans.length()-1]) {
+      ans.pop_back();
+    }
+    else {
+      ans.push_back(s[i]);
+    }
+  }
+  cout << s << " " << ans << endl;
+  return 0;
+}
