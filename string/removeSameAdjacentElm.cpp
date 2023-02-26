6d4bbb539671ea03d4ada6d8b2d9ea4ae8768b52
Shivam-Samant <shivamsamant000@gmail.com>
2023-02-26 12:52:35 +0530

    optimize code

--- string/removeSameAdjacentElm.cpp
+++ string/removeSameAdjacentElm.cpp
@@ -21,15 +21,15 @@ using namespace std;
 int main(){
   string s = "abbazza";
   string ans;
-  ans.push_back(s[0]);
-  for(int i = 1; i < s.length(); i++) {
-    if (ans.length()-1 >= 0 && s[i] == ans[ans.length()-1]) {
+  for(int i = 0; i < s.length(); i++) {
+    if (ans.length() > 0 && s[i] == ans[ans.length()-1]) {
       ans.pop_back();
     }
     else {
       ans.push_back(s[i]);
     }
   }
-  cout << s << " " << ans << endl;
+  cout << "Old string: " << s << endl;
+  cout << "New String:" << ans << endl;
   return 0;
 }
