e17386ec221b0482313ce3b7f56943cfa4918841
Shivam-Samant <shivamsamant000@gmail.com>
2023-03-11 14:22:44 +0530

    print all digits using recursion

---
+++ recursion/printAllDigit.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define IOS ios_base::sync_with_stdio(0)
+#define tie cin.tie(NULL),cout.tie(NULL)
+#define ll long long
+#define testcases ll T; cin >>T; while(T--)
+#define fi(i,a,b) for(ll i = a; i < b; i++)
+#define fd(i,a,b) for(ll i = a; i > b; i--)
+#define vc vector
+#define vi vc<int>
+#define vll vc<ll>
+#define pb push_back
+#define pii pair<int,int>
+#define mk make_pair
+#define all(p) p.begin(), p.end()
+#define si set<int>
+#define mii map<int, int>
+#define endl '\n'
+
+void printDigits(int n){
+  if(n == 0) return;
+  printDigits(n/10);
+  cout << n%10 << " ";
+}
+
+int main(){
+  int n = 1234;
+  printDigits(n);
+  return 0;
+}
