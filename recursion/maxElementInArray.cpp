4a2b99ce8ae9b81caaa8565b22eeafce69697e11
Shivam-Samant <shivamsamant000@gmail.com>
2023-03-11 13:37:31 +0530

    find and printed max element present in the array

---
+++ recursion/maxElementInArray.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define IOS ios_base::sync_with_stdio(0)
+#define tie cin.tie(NULL),cout.tie(NULL)
+#define ll long long
+#define testcases ll T; cin >>T; while(T--)
+#define fi(i,a,b) for(ll i = a; i < b; i++)
+#define fd(i,a,b) for(ll i = a; i > b; i--)
+#define vc vector
+#define vi vc<int>
+#define vll vc<ll>
+#define pb push_back
+#define pii pair<int,int>
+#define mk make_pair
+#define all(p) p.begin(), p.end()
+#define si set<int>
+#define mii map<int, int>
+#define endl '\n'
+
+int maxElement(int arr[], int N){
+  if(N == 1) return arr[0];
+  return max(arr[N-1], maxElement(arr, N-1));
+}
+
+int main(){
+  int arr[] = {5,40,20,10,2};
+  int N = 5;
+  cout << "Max element in array: " << maxElement(arr, N) << endl;
+  return 0;
+}
