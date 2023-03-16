898ad5d2c8dc90134107bf9cdcd1d32999d8ffe8
Shivam-Samant <shivamsamant000@gmail.com>
2023-03-16 22:50:34 +0530

    finding pivot in the array using binary search

--- searching/BinarySearch/findPivot.cpp
+++ searching/BinarySearch/findPivot.cpp
@@ -1,37 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int pivotIndex(vector<int>& nums) {
-        int s = 0;
-        int n = nums.size();
-        int e = n-1;
-        int mid = s+(e-s)/2;
+  int pivotIndex(vector<int> &nums)
+  {
+    int s = 0;
+    int n = nums.size();
+    int e = n - 1;
+    int mid = s + (e - s) / 2;
 
-        while(s < e) {
-            if (mid+1 < n && nums[mid] > nums[mid+1]) {
-                return mid;
-            }
-            if (mid-1 >= 0 && nums[mid-1] > nums[mid]) {
-                return mid-1;
-            }
+    while (s < e)
+    {
+      if (mid + 1 < n && nums[mid] > nums[mid + 1])
+      {
+        return mid;
+      }
+      if (mid - 1 >= 0 && nums[mid - 1] > nums[mid])
+      {
+        return mid - 1;
+      }
 
-            if (nums[s] < nums[mid]) {
-                s = mid + 1;
-            } else {
-                e = mid;
-            }
-						mid = s+(e-s)/2;
-        }
-        return s;
+      if (nums[s] < nums[mid])
+      {
+        s = mid + 1;
+      }
+      else
+      {
+        e = mid;
+      }
+      mid = s + (e - s) / 2;
     }
+    return s;
+  }
 };
 
-int main() {
-	vector<int> nums = {1,7,3,5,6,6};
-	Solution s;
-	cout << "Index of the pivot element is " << s.pivotIndex(nums) << endl;
-
+int main()
+{
+  vector<int> nums = {1, 7, 3, 5, 6, 6};
+  Solution s;
+  cout << "Index of the pivot element is " << s.pivotIndex(nums) << endl;
 }
