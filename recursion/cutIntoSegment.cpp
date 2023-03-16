ce0b828842cb914fd8e80865cc2dcde629aa4691
Shivam-Samant <shivamsamant000@gmail.com>
2023-03-16 23:24:29 +0530

    max cut of a segment problem is solved

---
+++ recursion/cutIntoSegment.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int maxWays(vector<int> &nums, int segmentLength) {
+  if (segmentLength == 0) {
+    return 0;
+  }
+  if (segmentLength < 0) {
+    return INT_MIN;
+  }
+
+  int maxi = INT_MIN;
+  for(int i = 0; i < nums.size(); i++) {
+    int ans = maxWays(nums, segmentLength-nums[i]);
+    if (ans != INT_MIN) {
+      maxi = max(maxi, ans+1);
+    }
+  }
+
+  return maxi;
+}
+
+int main() {
+  int segmentLength = 20;
+  vector<int> nums{5,2};
+  cout << "The maximum number of ways to cut the segment is " << maxWays(nums, segmentLength) << endl;
+
+  return 0;
+}
