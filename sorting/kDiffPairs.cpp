b3657a2f9fd088c960c7403b496a4b3eea91bca6
Shivam-Samant <shivamsamant000@gmail.com>
2023-03-18 13:10:03 +0530

    fix: distinct pair only

--- sorting/kDiffPairs.cpp
+++ sorting/kDiffPairs.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 #include<vector>
+#include<set>
 #include<algorithm>
 using namespace std;
 
-void solve(vector<int> &arr, int k, vector<pair<int, int>> &ans) {
+void solve(vector<int> &arr, int k, set<pair<int, int>> &ans) {
   sort(arr.begin(), arr.end());
 
   int i = 0, j = 1;
   while(i < arr.size() && j < arr.size()) {
     int diff = abs(arr[i] - arr[j]);
     if (diff == k) {
-      ans.push_back(make_pair(arr[i], arr[j]));
+      ans.insert(make_pair(arr[i], arr[j]));
       i++; j++;
     } else if (diff > k) {
       i++;
     } else {
       j++;
     }
+    if (i == j) j++;
   }
 }
 
 int main() {
-  vector<int> arr{3,1,4,1,5};
-  int k = 2;
-  vector<pair<int,int>> ans;
+  vector<int> arr{1,1,1,1,1};
+  int k = 0;
+  set<pair<int,int>> ans;
   solve(arr, k, ans);
 
   for(auto i: ans) {
