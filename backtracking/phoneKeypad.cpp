fcc166fdc9324b5b26a433fbd7a16e4ee9da3233
Shivam-Samant <shivamsamant000@gmail.com>
2023-03-26 12:06:18 +0530

    added edge case conditions

--- backtracking/phoneKeypad.cpp
+++ backtracking/phoneKeypad.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <map>
 using namespace std;
 
 string keypad[] = {
@@ -16,12 +15,20 @@ string keypad[] = {
 };
 
 void generateKeypadString(string &digits, int idx, string output) {
+  if (digits.length() == 0) {
+    cout << "Digits are empty" << endl;
+    return;
+  }
   if (idx == digits.length()) {
     cout << output << endl;
     return;
   }
 
   int num = digits[idx] - '0';
+  if (num < 2) {
+    cout << "Digits are not valid" << endl;
+    return;
+  }
   string val = keypad[num];
   for (int i = 0; i < val.length(); i++) {
     output.push_back(val[i]);
