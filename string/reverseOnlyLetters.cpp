b8f241074ca36934be08b9bca5fdfaa328005435
Shivam-Samant <shivamsamant000@gmail.com>
2023-03-29 21:07:06 +0530

    isalpha stl method used

--- string/reverseOnlyLetters.cpp
+++ string/reverseOnlyLetters.cpp
@@ -6,8 +6,8 @@ void revStr(string &s) {
     int i = 0;
     int j = s.size()-1;
     while (i < j) {
-        if (((s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122))) {
-            if (((s[j] >= 65 && s[j] <= 90) || (s[j] >= 97 && s[j] <= 122))) {
+        if (isalpha(s[i])) {
+            if (isalpha(s[j])) {
                 swap(s[i], s[j]);
                 i++;
                 j--;
