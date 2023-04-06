10e6d3e515272fedd0b2a19e4d2bbe41bc9a1ab5
Shivam-Samant <shivamsamant000@gmail.com>
2023-04-06 19:57:24 +0530

    resolving floating point number parameter

--- oops/Polymorphism/methodOverloading.cpp
+++ oops/Polymorphism/methodOverloading.cpp
@@ -19,11 +19,18 @@ class Adder {
     int sum (int x, int y, int z) {
         return x + y + z;
     }
+
+    int sum (int x, float y) {
+        return x + y;
+    }
 };
 
 int main() {
     Adder a;
     cout << a.sum(2,3) << endl;
     cout << a.sum(2,3,4) << endl;
+    // cout << a.sum(2, 3.5) << endl; // if we write 3.5 so the compiler understand it as double precision floating point number and it will give error
+    // to resolve the above error we use "f" to indicate that its a floting point number
+    cout << a.sum(2, 3.5f) << endl;
     return 0;
 }
