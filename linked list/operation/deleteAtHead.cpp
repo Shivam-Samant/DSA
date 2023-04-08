1ada3056e15f3f6de8576e707d99ae921df7afb1
Shivam-Samant <shivamsamant000@gmail.com>
2023-04-08 14:42:06 +0530

    remove constructor

--- linked list/operation/deleteAtHead.cpp
+++ linked list/operation/deleteAtHead.cpp
@@ -13,11 +13,6 @@ class Node {
     int data;
     Node* next;
 
-    Node() {
-        this->data = 0;
-        this->next = NULL;
-    }
-
     Node(int data) {
         this->data = data;
         this->next = NULL;
