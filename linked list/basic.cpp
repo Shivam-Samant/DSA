c026052513173e26976848f0fbfa62e8c9a102b4
Shivam-Samant <shivamsamant000@gmail.com>
2023-04-08 12:59:49 +0530

    code cleanups

--- linked list/basic.cpp
+++ linked list/basic.cpp
@@ -15,18 +15,6 @@ class Node {
 };
 
 int main() {
-    Node node1;
-    node1.data = 1;
-    node1.next = NULL;
-    Node node2;
-    node2.data = 2;
-    node1.next = &node2;
-    node2.next = NULL;
-
-    Node* head = &node1;
-    while(head != NULL) {
-        cout << head->data << endl;
-        head = head->next;
-    }
+    
     return 0;
 }
