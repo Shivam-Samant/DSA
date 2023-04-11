6c5a649ef4fd7a2ae9907d638dc45c11b61ccf35
Shivam-Samant <shivamsamant000@gmail.com>
2023-04-12 04:06:22 +0530

    reversed singly linked list

---
+++ linked list/reverseSinglyLL.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <climits>
+#include <stack>
+#include <queue>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    Node() {
+        this->data = 0;
+        this->next = NULL;
+    }
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void print(Node *ptr) {
+    while (ptr != NULL) {
+        cout << ptr->data << " ";
+        ptr = ptr->next;
+    }
+    cout << endl;
+}
+
+Node *reverseLL(Node *&head) {
+    Node *prev = NULL;
+    Node *curr = head;
+    while (curr!= NULL) {
+        Node *temp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = temp;
+    }
+    return prev;
+}
+
+Node *reverse(Node *prev, Node *curr) {
+    if (curr == NULL) {
+        return prev;
+    }
+
+    Node *temp = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = temp;
+    return reverse(prev, curr);
+}
+
+int main() {
+    Node *first = new Node(10);
+    Node *second = new Node(20);
+    Node *third = new Node(30);
+    Node *fourth = new Node(40);
+    Node *fifth = new Node(50);
+
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    print(first);
+
+    Node *head = first;
+
+    head = reverseLL(head);
+    print(head);
+    // head = reverse(NULL, head);
+    // print(head);
+    return 0;
+}
