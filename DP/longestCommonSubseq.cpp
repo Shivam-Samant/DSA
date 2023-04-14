1400f0c44ef6c3711ae799016941bf493298dee1
Shivam-Samant <shivamsamant000@gmail.com>
2023-04-14 11:46:13 +0530

    solved longest common subseq using bottom up DP

--- DP/longestCommonSubseq.cpp
+++ DP/longestCommonSubseq.cpp
@@ -28,10 +28,30 @@ int longestCommonSubsequence(string &s, string &r, int i, int j, vector<vector<i
     return dp[i][j] = ans;
 }
 
+// BOTTOM UP DP
+
+int solveTab(string &s, string &r) {
+    vector<vector<int>> dp(s.length()+1, vector<int>(r.length()+1, 0));
+
+    for (int i = s.length() - 1; i >= 0; i--) {
+        for (int j = r.length() - 1; j >= 0; j--) {
+            if (s[i] == r[j]) {
+                dp[i][j] = 1 + dp[i+1][j+1];
+            } else {
+                dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+            }
+        }
+    }
+    return dp[0][0];
+}
+
 int main() {
     string s = "abcdejsa;lfjas;kjf[ [pwje[rwjfodsjklfjalsjdflajiofjaf]]]";
     string r = "abcsjdlfjalskfp jawf joaiejfasdjflkjalfjeoihaind";
     vector<vector<int>> dp(s.length(), vector<int>(r.length(), -1));
+    cout << "TOP DOWN DP ANS: ";
     cout << longestCommonSubsequence(s, r, 0, 0, dp) << endl;
+    cout << "BOTTOM UP DP ANS: ";
+    cout << solveTab(s, r) << endl;
     return 0;
 }
